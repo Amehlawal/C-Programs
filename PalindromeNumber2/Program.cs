38e5580604eb40942eaf24635f8e53b81dbdc57f
Lawal Ameh <amehlawal@gmail.com>
2024-10-29 08:31:52 +0100

    Palindrome number 2

---
+++ PalindromeNumber2/Program.cs
@@ -0,0 +1,19 @@
+﻿int num, temp, revNum = 0, rem;
+
+Console.WriteLine("Enter a number : ");
+num = Convert.ToInt32(Console.ReadLine());
+temp = num;//121
+
+while(num > 0)
+{
+    rem = num % 10;
+    revNum = revNum * 10 + rem;
+    num = num / 10;
+}
+if (num == revNum)
+{
+    Console.WriteLine("Palindrome Number");
+}
+else
+    Console.WriteLine("Not a Palindrome Number");
+Console.ReadLine();
