c0ccea50fbbc6e6c25607694a0616258e7460765
Lawal Ameh <amehlawal@gmail.com>
2024-10-31 05:32:22 +0100

    Reverse String

---
+++ ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿string str, revString = "";
+
+Console.WriteLine("Enter string : ");
+
+str = Console.ReadLine();
+
+str = str.ToLower();
+Console.WriteLine(str.Length);
+for (int i =str.Length - 1; i >= 0; i--)
+{
+    revString = revString + str[i];
+}
+
+Console.WriteLine("Reverse string is {0}", revString);
+Console.ReadLine();
