ada1039393494506a1c6a7b2d956c78ff9417401
Lawal Ameh <amehlawal@gmail.com>
2024-11-01 10:00:23 +0100

    Display prime numbers

---
+++ DisplayPrimeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Please Enter desired max number to display prime numbers: ");
+int nMaxNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Prime numbers {0} and {1} are : ", 2, nMaxNumber);
+
+bool isPrimeNumber = true;
+
+for  (int i = 2; i < nMaxNumber; i++)
+{
+    for (int j = 2; j< nMaxNumber; j++)
+    {
+        if (i != j && i % j == 0)
+        {
+            isPrimeNumber = false; break;
+        }
+    }
+    if (isPrimeNumber)
+    {
+        Console.WriteLine(i);
+    }
+    isPrimeNumber = true;
+}
+
+Console.ReadLine();
