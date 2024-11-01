dab8416c150e74868beb03871e421989327d8bc3
Lawal Ameh <amehlawal@gmail.com>
2024-11-01 13:26:16 +0100

    Bubble Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] UnorderedNumbers = { 30, 10, -90, -40, 50, 25, 70, -54, -80, 38 };
+
+int nTemp = 0;
+
+Console.WriteLine("Sorted Numbers are : ");
+
+for (int i = 0; i < UnorderedNumbers.Length; i++)
+{
+    for (int j = i + 1; j < UnorderedNumbers.Length; j++)
+    {
+        if (UnorderedNumbers[i] > UnorderedNumbers[j])
+        {
+            nTemp = UnorderedNumbers[j];
+            UnorderedNumbers[j] = UnorderedNumbers[i];
+            UnorderedNumbers[i] = nTemp;
+        }
+    }
+    Console.WriteLine(UnorderedNumbers[i]);
+        
+}
+
+Console.ReadLine();
