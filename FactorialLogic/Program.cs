6e32fe8dfbd357420d2ecc2e3a9d18a3b5e62b4e
Lawal Ameh <amehlawal@gmail.com>
2024-11-01 14:58:33 +0100

    Factorial logic

---
+++ FactorialLogic/Program.cs
@@ -0,0 +1,22 @@
+﻿int number, factorial = 1, i;
+
+Console.WriteLine("Please enter the number to calculate the factorial : ");
+
+number = int.Parse(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine("Factorials are not applicable for negative number");
+}
+else if(number == 0)
+{
+    Console.WriteLine("Factorial of zero is 1");
+}
+else
+{
+    for (i = number; i >= 1; i--)
+    {
+        factorial = factorial * i;
+    }
+    Console.WriteLine("{0} factorial is  {1}", number.ToString(), factorial.ToString());
+}
