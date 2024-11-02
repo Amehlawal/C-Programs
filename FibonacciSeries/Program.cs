374cb4157e2a98616eeedd8034bab16a77a9e94f
Lawal Ameh <amehlawal@gmail.com>
2024-11-02 14:55:11 +0100

    fibobacci series

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int number, previousNumber = -1, nextNumber = 1;
+
+Console.WriteLine("Please enter the number which you would like finacci series ");
+
+number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Fibonacci Series ");
+
+for (int i = 0; i < number; i++)
+{
+    int sumNumber = previousNumber + nextNumber;
+    Console.WriteLine(sumNumber);
+    previousNumber = nextNumber;
+    nextNumber = sumNumber;
+}
+
+Console.ReadLine();
