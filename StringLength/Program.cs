bf633a19b2fa09eccff1e07041d5871982a7ed65
Lawal Ameh <amehlawal@gmail.com>
2024-11-02 14:54:42 +0100

    string length

---
+++ StringLength/Program.cs
@@ -0,0 +1,14 @@
+﻿string strSource = "";
+int nCount = 0;
+
+Console.WriteLine("Please enter the string : ");
+
+strSource = Console.ReadLine();
+
+foreach(char c in strSource)
+{
+    nCount = nCount + 1;
+}
+
+Console.WriteLine("{0} length is {1} ", strSource, nCount.ToString());
+Console.ReadLine();
