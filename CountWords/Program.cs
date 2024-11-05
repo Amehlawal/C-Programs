21270d42bc28cdf9604ccf7d3d39a8740e4293a1
Lawal Ameh <amehlawal@gmail.com>
2024-11-05 08:46:20 +0100

    count words

---
+++ CountWords/Program.cs
@@ -0,0 +1,16 @@
+﻿string str;
+int count = 1, len = 0;
+
+Console.WriteLine("Enter a string : ");
+str = Console.ReadLine();
+
+while (len<=str.Length-1)
+{
+    if(str[len] == ' ' || str[len]=='\n' || str[len]=='\t')
+    {
+        count++;
+    }
+    len++;
+}
+Console.WriteLine("Total No of word(s) "+ count);
+Console.ReadLine();
